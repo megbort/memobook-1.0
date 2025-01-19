6bc3e7391cdebdf98286fa11987ab13416aabeeb
Megan Krenbrink <megan.krenbrink@gmail.com>
2025-01-19 12:34:51 -0800

    Project clean up

---
+++ src/components/MainBanner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main-banner">
+    <h1 class="logo">MemoBook</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.main-banner {
+  background-color: var(--color-primary-dark);
+  color: var(--color-neutral-soft);
+  padding: 1rem;
+}
+</style>
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-page-content">
+    <MainBanner />
+    <main>
+      <p>Main contact information</p>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import MainBanner from '../components/MainBanner.vue'
+</script>
+
+<style scoped lang="scss">
+.main-page-content {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+main {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+}
+</style>
